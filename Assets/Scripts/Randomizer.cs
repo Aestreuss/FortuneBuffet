d7f93b3393859667ba1e3737e074da5c335ea52d
Aestreus <euphyxia@gmail.com>
2025-03-11 12:05:01 +1300

    lobter chance roll
    image rolling done, added rare percentage for lobster

--- Assets/Scripts/Randomizer.cs
+++ Assets/Scripts/Randomizer.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Randomizer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite[] sprites; //sprite array
+    public Sprite rareLobster;
+
+    public Image imageToChange;
+
+    public int rareLobsterChance;
+
+    public void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+            RandomizeImage();
+    }
+
+    void RandomizeImage()
+    {
+        int randomIndex = Random.Range(0, sprites.Length);
+        // if lobster is rolled
+        if (randomIndex == 4)
+        {
+            RareLobster(randomIndex);
+        }
+        else
+        {
+            imageToChange.sprite = sprites[randomIndex];
+        }
+            
+    }
+
+    void RareLobster(int randomIndex)
+    {
+        // rolls for chance to get different image
+        int rollForLobster = Random.Range(0, 100);
+        Debug.Log(rollForLobster);
+
+        // if chance is met, rare image !!
+        if(rollForLobster < rareLobsterChance)
+        {
+            imageToChange.sprite = rareLobster;
+        }
+        // otherwise idk
+        else
+        {
+            imageToChange.sprite = sprites[randomIndex];
+        }
     }
 }
