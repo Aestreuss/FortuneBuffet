1effb001307bd6057ded6909bde456e10da6a46d
Aestreuss <euphyxia@gmail.com>
2025-03-17 23:01:31 +1300

    menu screen done

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Button exitButton;
+    public Button startButton;
+
+    private void Start()
+    {
+        exitButton.onClick.AddListener(ExitGame);
+        startButton.onClick.AddListener(StartGame);
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Buffet");
+    }
+
+}
